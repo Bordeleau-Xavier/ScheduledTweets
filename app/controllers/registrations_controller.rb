5a7f20749c0d8e03f9290d8cb4d52e9c145ff8f5
Bordeleau-Xavier <xavierbordeleau@live.ca>
2024-02-27 19:27:47 -0500

    add registration error handling

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,16 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    print("asdf")
-    render plain: "thanks!"
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Successfully created account"
+    else
+      #flash[:alert] = "Something went wrong"
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
